bd128a9acd545585538d09eddd14108fdb305769
polyakov-artem <polyakov.contact@yandex.ru>
2024-03-18 08:18:55 +0300

    feat: implement removeProperties, compareObjects functions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -62,8 +62,16 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const result = obj;
+
+  keys.forEach((key) => {
+    if (key in result) {
+      delete result[key];
+    }
+  });
+
+  return result;
 }
 
 /**
@@ -78,8 +86,15 @@ function removeProperties(/* obj, keys */) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  function stringify(obj) {
+    return JSON.stringify(obj);
+  }
+
+  if (stringify(obj1) === stringify(obj2)) {
+    return true;
+  }
+  return false;
 }
 
 /**
